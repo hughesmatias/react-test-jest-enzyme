0bad94ee2c4f1cfe2b31c7b2706d58cf6459a005
Matias Perez Hughes <hughesmatias@gmail.com>
2020-10-12 17:30:39 -0300

    before is a function to excute in each test

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -3,9 +3,12 @@ import { shallow } from 'enzyme';
 import Header from './index';
 
 describe('Header test', () => {
+  let comp;
+  beforeEach(() => {
+    comp = shallow(<Header />);
+  });
+
   it('render ok', () => {
-    const comp = shallow(<Header />);
-    
     expect(
       comp.find('img').length + comp.find('a').length
     ).toBe(2);
